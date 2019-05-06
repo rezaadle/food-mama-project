2b81e7d07979241f899e0dd56bc8c81f8017a894
Reza Adle <reza.adle@technobconsulting.com>
2019-05-06 13:40:38 +0200

    Add markdown

--- mama.py
+++ mama.py
@@ -7,19 +7,15 @@ Created on Wed May  1 11:52:15 2019
 """
 
 import pandas as pd
-import numpy as np
 import os
 
-general_path = "/Users/Reza/Courses/foodMama/"
-folder = "datasets"
-path = general_path + folder
+path = "datasets/"
 files = os.listdir(path)
 
 dfs = []
 for file in files:
     filename = file.split("_")
     dfs.append(filename[0])
-    exec(dfs[-1] + '= pd.read_csv(folder + "/" + file)')
+    exec(dfs[-1] + '= pd.read_csv(path + file)')
 
-exec("print(" + dfs[1] + ".head())")
-
+print("Name of the dataframes created: ", *dfs, sep="\n")
